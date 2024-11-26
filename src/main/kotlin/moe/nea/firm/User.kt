457d4eaf10e5bb80b576af651f5babaae63673da
Linnea Gräf <nea@nea.moe>
2024-11-26 22:49:12 +0100

    Add alternative crasher

--- src/main/kotlin/moe/nea/firm/User.kt
+++ src/main/kotlin/moe/nea/firm/User.kt
@@ -1,9 +1,13 @@
 package moe.nea.firm
 
 import ext.StringIdentifiable
+import kotlin.enums.enumEntries
 
 object User : ManagedConfig("test") {
 	val option by choice("name") { TestEnum.A }
+	val optionAlt by choice("name", enumEntries<TestEnum>(),
+	                        StringIdentifiable.createCodec { enumValues<TestEnum>() },
+	                        EnumRenderer.default<TestEnum>()) { TestEnum.A }
 
 	enum class TestEnum : StringIdentifiable {
 		A, B, C;
